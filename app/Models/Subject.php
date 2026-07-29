06b50738ed3782b427157072f4ea021463b85f5c
Sky-last <babangarif2403@gmail.com>
2026-07-29 09:32:58 +0700

    update code

---
+++ app/Models/Subject.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Subject extends Model
+{
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    protected $casts = ['is_active' => 'boolean'];
+
+    // relasi 1 to many dengan tabek question
+    public function question(): HasMany
+    {
+        return $this->hasMany(Question::class);
+    }
+
+}
---
+++ database/migrations/2026_07_29_085549_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Subject;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            // foreign key untuk relasi ke table subject
+            $table->foreignIdFor(Subject::class)->constrained()->cascadeOnDelete();
+            $table->longText('payload');
+            $table->text('description')->nullable();
+            $table->boolean('is_active')->default(true);
+            // kolom soft-delete 'deleted_at'
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
